da36cf7ce47697cc6c7fa1adf80d596ffa2c0497
George Hotz <geohot@gmail.com>
2014-12-15 09:03:13 -0500

    dont panic still

--- vmx.c
+++ vmx.c
@@ -5,7 +5,14 @@
 // needed for hacks
 unsigned long __force_order;
 
+struct vmcs {
+  u32 revision_id;
+  u32 abort;
+  char data[0];
+};
+
 unsigned char vmcs[4096] __attribute__ ((aligned (4096)));
+struct vmcs *this_vmcs = (struct vmcs *)vmcs;
 
 #include <asm/vmx.h>
 #include <asm/processor-flags.h>
@@ -31,7 +38,18 @@ typedef struct pmap *pmap_t;
 extern pmap_t kernel_pmap;
 extern ppnum_t pmap_find_phys(pmap_t pmap, addr64_t va);
 
+#define rdmsr(msr, low, high)                                   \
+  do {                                                            \
+    u64 __val = native_read_msr((msr));                     \
+    (void)((low) = (u32)__val);                             \
+    (void)((high) = (u32)(__val >> 32));                    \
+  } while (0)
+
 void hardware_enable() {
+  u32 vmx_msr_low, vmx_msr_high;
+  rdmsr(MSR_IA32_VMX_BASIC, vmx_msr_low, vmx_msr_high);
+
+  this_vmcs->revision_id = vmx_msr_low;
   u64 phys = pmap_find_phys(kernel_pmap, vmcs);
   printf("vmcs @ %p @ %p\n", vmcs, phys);
 
